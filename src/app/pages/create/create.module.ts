330e3b923b4dc81d09e44dd62796395497983790
riquemath <fredabisai@gmail.com>
2020-02-10 13:30:19 +0300

    feat(create.module.ts): make a create scorecard module

---
+++ src/app/pages/create/create.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateComponent } from './create.component';
+import { CreateHeaderComponent } from './components/header/header.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SampleScorecardComponent } from './components/sample-scorecard/sample-scorecard.component';
+import { TitleAreaComponent } from './components/title-area/title-area.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateComponent
+  }
+];
+
+@NgModule({
+  declarations: [CreateComponent, CreateHeaderComponent, SampleScorecardComponent, TitleAreaComponent],
+  imports: [
+    CommonModule, 
+    RouterModule.forChild(routes),
+    SharedModule
+  ]
+})
+export class CreateModule { }
