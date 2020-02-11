e4a83d995e2208b16fbcc07c08e79def497dc049
riquemath <fredabisai@gmail.com>
2020-02-11 09:33:24 +0300

    feat(samplescorecard): make settings actions buttons

--- src/app/pages/create/components/sample-scorecard/sample-scorecard.component.ts
+++ src/app/pages/create/components/sample-scorecard/sample-scorecard.component.ts
@@ -1,16 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  selection: string;
-  description: string;
-  type: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {selection: 'Groups', description: 'Hydrogen', type: 'group'},
-  {selection: 'Starting Period', description: 'Hydrogen', type: 'period'},
-  {selection: 'Organisation Unit', description: 'Hydrogen', type: 'organisation-unit'},
-];
-
 
 @Component({
   selector: 'app-sample-scorecard',
@@ -18,8 +6,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./sample-scorecard.component.css']
 })
 export class SampleScorecardComponent implements OnInit {
-  displayedColumns: string[] = ['selection', 'description'];
-  dataSource = ELEMENT_DATA;
+  
   constructor() { }
 
   ngOnInit() {
