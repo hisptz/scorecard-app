b3d417c5052366c5ca823d7c4ee55536a15cd143
riquemath <fredabisai@gmail.com>
2020-02-17 16:51:41 +0300

    Move options buttons from sample-scorecard component to create-component

--- src/app/pages/create/components/sample-scorecard/sample-scorecard.component.ts
+++ src/app/pages/create/components/sample-scorecard/sample-scorecard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { SharingDialogComponent } from 'src/app/shared/dialogs/sharing-dialog/sharing-dialog.component';
-import { PeriodDialogComponent } from 'src/app/shared/dialogs/period-dialog/period-dialog.component';
-import { OrganisationUnitDialogComponent } from 'src/app/shared/dialogs/organisation-unit-dialog/organisation-unit-dialog.component';
 
 @Component({
   selector: 'app-sample-scorecard',
@@ -10,23 +6,9 @@ import { OrganisationUnitDialogComponent } from 'src/app/shared/dialogs/organisa
   styleUrls: ['./sample-scorecard.component.css']
 })
 export class SampleScorecardComponent implements OnInit {
-  constructor(private dialog: MatDialog) { }
+  constructor() { }
 
   ngOnInit() {
   }
-  openSharingDialog(): void {
-    const dialogRef = this.dialog.open(SharingDialogComponent, {
-      width: '250px'
-    });
-  }
-  openPeriodDialog(): void {
-    const dialogRef = this.dialog.open(PeriodDialogComponent, {
-      width: '250px'
-    });
-  }
-  openOrgUnitDialog() {
-    const dialogRef = this.dialog.open(OrganisationUnitDialogComponent, {
-      width: '250px'
-    });
-  }
+  
 }
