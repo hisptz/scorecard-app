8d075889ebe314f00821418a9af81bfcfa0ab7fa
riquemath <fredabisai@gmail.com>
2020-02-18 17:12:30 +0300

    Add routes in view module

--- src/app/pages/view/view.module.ts
+++ src/app/pages/view/view.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewComponent } from './view.component';
+import { ViewHeaderComponent } from './components/view-header/view-header.component';
+import { ViewTitleAreaComponent } from './components/view-title-area/view-title-area.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewComponent
+  }
+];
 
 @NgModule({
-  declarations: [ViewComponent],
+  declarations: [ViewComponent, ViewHeaderComponent, ViewTitleAreaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class ViewModule { }
