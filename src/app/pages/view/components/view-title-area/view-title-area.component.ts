6067d417219e426b8f6c6b05462ae433a3a897bb
riquemath <fredabisai@gmail.com>
2020-02-18 17:10:20 +0300

    Make title area of a view

---
+++ src/app/pages/view/components/view-title-area/view-title-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-title-area',
+  templateUrl: './view-title-area.component.html',
+  styleUrls: ['./view-title-area.component.css']
+})
+export class ViewTitleAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
