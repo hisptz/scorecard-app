c6d354b2c4e750a285768139c161ced196b3ca02
riquemath <fredabisai@gmail.com>
2020-02-18 15:24:09 +0300

    Generate view component

--- src/app/pages/view/view.module.ts
+++ src/app/pages/view/view.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewComponent } from './view.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewComponent],
   imports: [
     CommonModule
   ]
