6df00268065176b32558bae6f91f26d3217fc535
Rajab Mkomwa <rajeyw@gmail.com>
2020-05-21 01:35:34 +0300

    feat: add mock up for basic scorecard configuration

--- src/app/pages/manage/containers/manage/manage.component.ts
+++ src/app/pages/manage/containers/manage/manage.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+  styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
+  public Editor = ClassicEditor;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/manage/manage.module.ts
+++ src/app/pages/manage/manage.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ManageRoutingModule } from './manage-routing.module';
-import { ManageComponent } from './containers/manage/manage.component';
-import { ManageHeaderComponent } from './components/manage-header/manage-header.component';
+import { NgModule } from '@angular/core';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { SharedModule } from 'src/app/shared/shared.module';
+
+import { ManageHeaderComponent } from './components/manage-header/manage-header.component';
+import { ManageComponent } from './containers/manage/manage.component';
+import { ManageRoutingModule } from './manage-routing.module';
 
 @NgModule({
   declarations: [ManageComponent, ManageHeaderComponent],
-  imports: [CommonModule, ManageRoutingModule, SharedModule],
+  imports: [CommonModule, ManageRoutingModule, SharedModule, CKEditorModule],
 })
 export class ManageModule {}
