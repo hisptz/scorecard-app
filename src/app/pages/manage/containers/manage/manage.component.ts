3ca4ec030970188366fca553f9000f1206d38ed1
Rajab Mkomwa <rajeyw@gmail.com>
2020-07-30 10:23:20 +0300

    feat: add management pages

--- src/app/pages/manage/containers/manage/manage.component.ts
+++ src/app/pages/manage/containers/manage/manage.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 @Component({
   selector: 'app-manage',
@@ -9,6 +8,31 @@ import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64u
 })
 export class ManageComponent implements OnInit {
   public Editor = ClassicEditor;
+  legendDefinitions = [
+    {
+      id: 1,
+      name: 'Target achieved / on track',
+      color: '#008000',
+      startValue: 67,
+      endValue: 100,
+    },
+    {
+      id: 2,
+      name: 'Progress, but more effort required',
+      color: '#FFFF00',
+      startValue: 33,
+      endValue: 67,
+    },
+    {
+      id: 3,
+      name: 'Not on track',
+      color: '#FF0000',
+      startValue: 0,
+      endValue: 33,
+    },
+    { id: 4, name: 'N/A', color: '#D3D3D3', default: true },
+    { id: 5, name: 'No data', color: '#FFFFFF', default: true },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
