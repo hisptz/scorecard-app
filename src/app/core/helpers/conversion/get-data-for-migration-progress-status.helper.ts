39bd1ae7a3ba0d4f4a5569521b25f828ab51f1a9
riquemath <fredabisai@gmail.com>
2020-08-13 14:03:30 +0300

    fix: make a separate function to get message

--- src/app/core/helpers/conversion/get-data-for-migration-progress-status.helper.ts
+++ src/app/core/helpers/conversion/get-data-for-migration-progress-status.helper.ts
@@ -4,83 +4,78 @@ export function getMessageForMigrationProgressStatus(
   stage: ScorecardMigrationStage,
   customValue?: any
 ): string {
-  let message = '';
   const scorecardName =
     customValue && typeof customValue === 'string' ? customValue : '';
+  const numberOfOldScorecards =
+    customValue && typeof customValue === 'number' ? customValue : 0;
+  const id = customValue ? customValue : '';
+  const message = getMessage(stage, {
+    scorecardName,
+    numberOfOldScorecards,
+    id,
+  });
+  return message;
+}
+
+function getMessage(
+  stage: ScorecardMigrationStage,
+  { scorecardName, numberOfOldScorecards, id }
+): string {
+  let message = '';
   switch (stage) {
-    case ScorecardMigrationStage.CHECK_OLD_SCORECARDS: {
+    case ScorecardMigrationStage.CHECK_OLD_SCORECARDS:
       message = 'Checking for old scorecard data';
       break;
-    }
-    case ScorecardMigrationStage.ERROR_LOADING_OLD_SCORECARDS: {
+    case ScorecardMigrationStage.ERROR_LOADING_OLD_SCORECARDS:
       message = 'There was an error while loading old scorecards';
       break;
-    }
-    case ScorecardMigrationStage.NONE_OLD_SCORECARD_PRESENT: {
+    case ScorecardMigrationStage.NONE_OLD_SCORECARD_PRESENT:
       message = 'There is no old scorecard present';
       break;
-    }
-    case ScorecardMigrationStage.OLD_SCORECARDS_PRESENT: {
-      const numberOfOldScorecards =
-        customValue && typeof customValue === 'number' ? customValue : 0;
+    case ScorecardMigrationStage.OLD_SCORECARDS_PRESENT:
       message = `There are ${numberOfOldScorecards} old scorecards present`;
       break;
-    }
-    case ScorecardMigrationStage.START_MIGRATION: {
+    case ScorecardMigrationStage.START_MIGRATION:
       message = 'Scorecard data migration process has started';
       break;
-    }
-    case ScorecardMigrationStage.ERROR_MIGRATION: {
+    case ScorecardMigrationStage.ERROR_MIGRATION:
       message =
-        'Scorecards data for migration are missing, Exiting migration process...';
+        'Scorecards data for migration are missing, Exiting migration process';
       break;
-    }
-    case ScorecardMigrationStage.FORMAT_OLD_SCORECARDS: {
+    case ScorecardMigrationStage.FORMAT_OLD_SCORECARDS:
       message = 'Converting old scorecards data to a new format';
       break;
-    }
-    case ScorecardMigrationStage.FORMAT_SPECIFIC_OLD_SCORECARD: {
+    case ScorecardMigrationStage.FORMAT_SPECIFIC_OLD_SCORECARD:
       message = `Formatting scorecard ${scorecardName}`;
       break;
-    }
-    case ScorecardMigrationStage.ERROR_FORMAT_SPECIFIC_OLD_SCORECARD: {
-      const id = customValue ? customValue : '';
+    case ScorecardMigrationStage.ERROR_FORMAT_SPECIFIC_OLD_SCORECARD:
       message = `There is no data in Scorecard with id ${id}`;
       break;
-    }
-    case ScorecardMigrationStage.SAVE_LIST_ITEM: {
+    case ScorecardMigrationStage.SAVE_LIST_ITEM:
       message = `Scorecard ${scorecardName} saved in List items`;
       break;
-    }
-    case ScorecardMigrationStage.SAVE_DETAIL_ITEM: {
+    case ScorecardMigrationStage.SAVE_DETAIL_ITEM:
       message = `Scorecard ${scorecardName} saved in Detail items`;
       break;
-    }
-    case ScorecardMigrationStage.ERROR_SAVE_LIST_ITEM: {
-      message = `Saving list item '${scorecardName}' failed, migration continues with other scorecards...`;
+    case ScorecardMigrationStage.ERROR_SAVE_LIST_ITEM:
+      message = `Saving list item '${scorecardName}' failed, migration continues with other scorecards`;
       break;
-    }
-    case ScorecardMigrationStage.ERROR_SAVE_DETAIL_ITEM: {
-      message = `Saving detail item '${scorecardName}' failed, migration continues with other scorecards...`;
+    case ScorecardMigrationStage.ERROR_SAVE_DETAIL_ITEM:
+      message = `Saving detail item '${scorecardName}' failed, migration continues with other scorecards`;
       break;
-    }
-    case ScorecardMigrationStage.DELETE_OLD_SCORECARD: {
+    case ScorecardMigrationStage.DELETE_OLD_SCORECARD:
       message = `Deleted scorecard '${scorecardName}' from old scorecard list`;
       break;
-    }
-    case ScorecardMigrationStage.ERROR_DELETE_OLD_SCORECARD: {
+    case ScorecardMigrationStage.ERROR_DELETE_OLD_SCORECARD:
       message = `Deleting scorecard '${scorecardName}' from old scorecard list failed`;
       break;
-    }
-    case ScorecardMigrationStage.END_MIGRATION: {
+    case ScorecardMigrationStage.END_MIGRATION:
       message =
         'The migration process has ended, Please see the process summary';
       break;
-    }
-    default: {
+    default:
       message = 'Scorecard data migration is in progress';
       break;
-    }
   }
   return message;
 }
