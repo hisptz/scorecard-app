6bb14488bc34f1f55ebdc6142e43e005a1503e21
rajey <rajeyw@gmail.com>
2021-06-27 17:41:47 +0300

    add e2e test for scorecard listing & update testing configuration

---
+++ cypress/integration/0.2-List-Scorecards/list-scorecard.js
@@ -0,0 +1,34 @@
+/**
+ * Scenario: Accessing Scorecard List
+ */
+
+Given("authorized department officer", () => {});
+
+When("opening a list of available scorecards", () => {});
+
+Then(
+  "I should be presented with a list of already configured scorecards",
+  () => {}
+);
+
+/**
+ * Scenario: Accessing many scorecards
+ */
+Given("authorized department officer", () => {});
+When("opening a list of many scorecards", () => {});
+Then("I should be presented with a chunked list of scorecards", () => {});
+And(
+  "I should be able to navigate through chunks to view more scorecards",
+  () => {}
+);
+
+/**
+ * Scenario: Accessing scorecard empty list
+ */
+Given("an authorized department officer", () => {});
+When("opening a list of available scorecards", () => {});
+And("there are no configured scorecards", () => {});
+Then(
+  'I should be presented with a message "There are no configured scorecards"',
+  () => {}
+);
