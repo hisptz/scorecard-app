d8e47ead01e4a4bb3de0d7eeb8b5a31bfb0619af
rajey <rajeyw@gmail.com>
2021-07-18 01:23:42 +0300

    derive data selections from scorecard object

--- src/shared/services/getScorecardData.js
+++ src/shared/services/getScorecardData.js
@@ -1,27 +1,37 @@
 import { Period } from "@iapps/period-utilities";
+import { flatten } from "lodash";
 export default async function getScorecardData(scorecard) {
   const selectedOrgUnits = _getSelectedOrgUnits(scorecard.orgUnitSelection);
   const { currentPeriods, previousPeriods } = _getSelectedPeriods(
     scorecard.periodSelection,
     scorecard.periodType
   );
+  const selectedData = _getSelectedData(scorecard.dataSelection);
 
   return Promise.all([
-    _getScorecardData({ selectedOrgUnits, selectedPeriods: currentPeriods }),
-    _getScorecardData({ selectedOrgUnits, selectedPeriods: previousPeriods }),
+    _getScorecardData({
+      selectedOrgUnits,
+      selectedPeriods: currentPeriods,
+      selectedData,
+    }),
+    _getScorecardData({
+      selectedOrgUnits,
+      selectedPeriods: previousPeriods,
+      selectedData,
+    }),
   ]);
 }
 
 function _getScorecardData(selections) {
-  const { selectedOrgUnits, selectedPeriods } = selections;
-  console.log(selectedOrgUnits, selectedPeriods);
+  const { selectedOrgUnits, selectedPeriods, selectedData } = selections;
+  console.log(selectedOrgUnits, selectedPeriods, selectedData);
 
-  // TODO Set Data selection
   return new Promise((resolve, reject) =>
     resolve({ data: { value: "previous" } })
   );
 }
 
+// TODO Derive selected period from period selection if supplied
 function _getSelectedPeriods(periodSelection, periodType) {
   try {
     const periods = new Period()?.setType(periodType)?.get()?.list();
@@ -49,4 +59,24 @@ function _getSelectedOrgUnits(orgUnitSelection) {
     orgUnitString = orgUnitSelection?.orgUnitIds;
 
   return orgUnitString;
+}
+
+function _getSelectedData(dataSelection) {
+  const selectedItems = flatten(
+    dataSelection?.dataGroups.map((dataGroup) =>
+      flatten(
+        (dataGroup?.dataHolders || []).map(
+          (dataHolder) => dataHolder.dataSources
+        )
+      )
+    )
+  );
+  return {
+    normalDataItems: selectedItems.filter(
+      (selectedItem) => selectedItem.type !== "functionRule"
+    ),
+    customDataItems: selectedItems.filter(
+      (selectedItem) => selectedItem.type === "functionRule"
+    ),
+  };
 }
