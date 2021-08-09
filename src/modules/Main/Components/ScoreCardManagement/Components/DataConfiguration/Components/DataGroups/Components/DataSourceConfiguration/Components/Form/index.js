0b689727e5748594785c8bace8eb650039483999
nnkogift <nnkogift@hotmail.com>
2021-08-09 15:35:14 +0300

    fix(DataSourceConfiguration): Fixes
    Issues with data configuration not appearing on data source select

--- src/modules/Main/Components/ScoreCardManagement/Components/DataConfiguration/Components/DataGroups/Components/DataSourceConfiguration/Components/Form/index.js
+++ src/modules/Main/Components/ScoreCardManagement/Components/DataConfiguration/Components/DataGroups/Components/DataSourceConfiguration/Components/Form/index.js
@@ -2,7 +2,11 @@ import {cloneDeep, set} from 'lodash'
 import React from 'react'
 import {useRecoilState, useRecoilValue} from "recoil";
 import ScorecardIndicator from "../../../../../../../../../../../../core/models/scorecardIndicator";
-import {ScorecardConfigEditState, ScorecardConfigDirtyState} from "../../../../../../../../../../../../core/state/scorecard";
+import {
+    ScorecardConfigEditState,
+    ScorecardConfigDirtyState,
+    ScorecardConfigDirtySelector
+} from "../../../../../../../../../../../../core/state/scorecard";
 import DataSourceConfigurationForm
     from "../../../../../../../../../../../../shared/Components/CustomForm/components/DataSourceConfigurationForm";
 
@@ -11,8 +15,8 @@ export default function SelectedDataSourceConfigurationForm() {
     const legendDefinitions = useRecoilValue(ScorecardConfigDirtyState('legendDefinitions'))
     const selectedGroupIndex = scorecardEditState?.selectedGroupIndex;
     const selectedDataHolderIndex = scorecardEditState?.selectedDataHolderIndex;
-    const path = ['dataSelection', 'dataGroups', selectedGroupIndex, 'dataHolders', selectedDataHolderIndex]
-    const [selectedDataHolder, updateSelectedDataHolder] = useRecoilState(ScorecardConfigDirtyState(path));
+    const path = ['dataGroups', selectedGroupIndex, 'dataHolders', selectedDataHolderIndex]
+    const [selectedDataHolder, updateSelectedDataHolder] = useRecoilState(ScorecardConfigDirtySelector({key: 'dataSelection',path}));
 
     const onFormChange = (index) => ({values, dirty}) => {
         if (dirty) {
