1798d779ed1180d141943a00e017d01567612372
nnkogift <nnkogift@hotmail.com>
2021-08-10 13:20:09 +0300

    fix(Scorecard Management ): Fixes
    Additional labels inclusion to scorecard model

--- src/core/models/scorecard.js
+++ src/core/models/scorecard.js
@@ -27,7 +27,8 @@ export default class Scorecard extends SavableDataModel {
             userGroupAccesses: [],
             userAccesses: [],
             user: {},
-            targetOnLevels: false
+            targetOnLevels: false,
+            additionalLabels: []
         }
     }
 
