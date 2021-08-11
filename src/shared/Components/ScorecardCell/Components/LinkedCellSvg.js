303b8fb69f74c7b4a16604097423c2dd057dd23a
nnkogift <nnkogift@hotmail.com>
2021-08-11 10:10:28 +0300

    fix(ScorecardCells): Fixes
    Set dynamic font size to svg texts based on the value's length

--- src/shared/Components/ScorecardCell/Components/LinkedCellSvg.js
+++ src/shared/Components/ScorecardCell/Components/LinkedCellSvg.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, {useMemo} from "react";
 import {DecreasingArrows, IncreasingArrows} from "./Arrows";
 
 
@@ -14,19 +14,21 @@ export default function LinkedCellSvg({
 
     const width = 100;
     const height = 47;
-    const fontSize = 12;
+    const fontSize=12;
+    const topFontSize = useMemo(() => topValue?.length > 4 ? 12 : 14, [topValue]);
+    const bottomFontSize = useMemo(() => bottomValue?.length > 4 ? 12 : 14, [bottomValue]);
     const padding = 6;
 
     return (
         <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} style={{display: 'block'}}>
             <polygon points={`0,0 0,${height} ${width}, 0`}
                      style={{fill: topColor ?? '#FFFFFF', strokeWidth: 1, stroke: 'rgb(232, 237, 242)'}}/>
-            <text lengthAdjust="spacingAndGlyphs" textLength={28} fontSize={fontSize} x={padding + fontSize} y={fontSize + padding}>{topValue}</text>
+            <text lengthAdjust="spacingAndGlyphs" textLength={topValue?.length > 4 ? 28 : null}  fontSize={topFontSize} x={padding + fontSize} y={fontSize + padding}>{topValue}</text>
             {topStatus && (topStatus === 'increasing' ? <IncreasingArrows fontSize={fontSize} y={padding} x={padding + fontSize/2}/> :
                 <DecreasingArrows fontSize={fontSize}  y={padding + fontSize} x={padding + fontSize/2}/>)}
             <polygon points={`${width},0 ${width},${height} 0,${height}`}
                      style={{fill: bottomColor ?? '#FFFFFF', strokeWidth: 1, stroke: 'rgb(232, 237, 242)'}}/>
-            <text lengthAdjust="spacingAndGlyphs" textLength={28} fontSize={fontSize} x={width/2 + padding/4} y={height - padding}>{bottomValue}</text>
+            <text lengthAdjust="spacingAndGlyphs" textLength={bottomValue?.length > 4 ? 28 : null} fontSize={bottomFontSize} x={width/2 + padding/4} y={height - padding}>{bottomValue}</text>
             {bottomStatus && (bottomStatus === 'increasing' ? <IncreasingArrows fontSize={fontSize} y={height - (fontSize + padding)} x={width - (padding + fontSize/2)}/> :
                 <DecreasingArrows fontSize={fontSize} y={height - padding} x={width - (padding + fontSize/2)}/>)}
         </svg>
