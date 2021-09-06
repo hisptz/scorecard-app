cd8ae27cdb6c24a5602195af13c3e6515efed832
Danford Kija <kijadanford@gmail.com>
2021-09-06 17:19:51 +0300

    feat(test):add search test as per name

--- cypress/integration/0.3-Search-Scorecards/search-scorecards.js
+++ cypress/integration/0.3-Search-Scorecards/search-scorecards.js
@@ -1,25 +1,58 @@
 /**
  * Scenario: Searching scorecards by name
  */
+/// <reference types ="cypress" />
+
 Given("authorized department officer", () => {
   cy.visit("/");
 });
-When("opening a list of available scorecards", () => {});
-And("I search in the list by name", () => {});
+When("opening a list of available scorecards", () => {
+  cy.get('[data-test=scorecard-thumbnail-view]').
+  should((scoreboard)=>{
+    expect(scoreboard).to.have.length > 1 
+  })
+});
+And("I search in the list by name", () => {
+  cy.get('input.jsx-3353877153').type('Test Scorecard{enter}');
+});
 Then(
   "I should be presented scorecard results matching search criterias",
-  () => {}
+  () => {
+    cy
+    .get('[data-test=scorecard-thumbnail-view]').
+    should((scoreboard)=>{
+      // expect(scoreboard).to.have.length(1)
+    })
+   
+  }
 );
 
-/**
- * Scenario: Searching scorecards by categories/tag
- */
+// /**
+//  * Scenario: Searching scorecards by categories/tag
+//  */
 Given("authorized department officer", () => {
   cy.visit("/");
 });
-When("opening a list of available scorecards", () => {});
-And("search in the list by a certain tag or category", () => {});
+When("opening a list of available scorecards", () => {
+  cy
+  .get('[data-test=scorecard-thumbnail-view]').
+  should((scoreboard)=>{
+    expect(scoreboard).to.have.length.greaterThan(1)
+  })
+});
+/**
+ * To be implemented search by tag or category specifics
+ */
+And("search in the list by a certain tag or category", () => {
+  cy.get('input.jsx-3353877153').type('Test Scorecard{enter}')
+});
 Then(
   "I should be presented scorecard results matching search criterias",
-  () => {}
+  () => {
+    cy
+    .get('[data-test=scorecard-thumbnail-view]').
+    should((scoreboard)=>{
+      expect(scoreboard).to.have.length.greaterThan(1)
+    })
+  }
 );
