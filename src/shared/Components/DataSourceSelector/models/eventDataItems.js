d77751d087da9df3760e9debad7f9fda272bf281
nnkogift <nnkogift@hotmail.com>
2021-09-14 11:15:01 +0300

    fix(Data Source Selector): Fix
    - Changed the api endpoint for event data items to include old apis that
    do not have the dataItems api endpoint

--- src/shared/Components/DataSourceSelector/models/eventDataItems.js
+++ src/shared/Components/DataSourceSelector/models/eventDataItems.js
@@ -1,11 +1,73 @@
+import {filter as _filter, isEmpty} from "lodash";
 import NativeDataSource from "./nativeDataSource";
 
 
 export default class EventDataItems extends NativeDataSource {
 
-    async getDataSources(engine, {page, filter}) {
-        const finalFilter = [...(filter ?? []), `dimensionItemType:${this.filterType}:${this.dimensionItemType}`, `valueType:eq:NUMBER`]
-        return super.getDataSources(engine, {page, filter: finalFilter});
+    constructor() {
+        super({
+            label: 'Event Data Items',
+            resource: 'programTrackedEntityAttributes',
+            dimensionItemType: '[PROGRAM_DATA_ELEMENT,PROGRAM_ATTRIBUTE]',
+            filterType: 'in',
+            groupKey: 'programId',
+            groupResource: 'programs',
+            type: 'programDataItem'
+
+        });
+
+        this.dataSourcesQuery = {
+            attributes: {
+                resource: 'programs',
+                id: ({id}) => id,
+                params: {
+                    fields: [
+                        'id',
+                        'programTrackedEntityAttributes[trackedEntityAttribute[id,displayName,valueType]]',
+                    ]
+                }
+            },
+            dataElements: {
+                resource: 'programDataElements',
+                params: ({id, page}) => ({
+                    program: id,
+                    page,
+                    filter: [
+                        `valueType:eq:NUMBER`
+                    ],
+                    fields: [
+                        'dataElement[id,displayName]',
+                        'displayName',
+                        'valueType'
+                    ]
+                })
+            }
+        }
     }
 
+    async getDataSources(engine, {page, filter}) {
+        if (!isEmpty(filter)) {
+            const response = await engine?.query(this.dataSourcesQuery, {
+                variables: {
+                    page,
+                    id: filter
+                }
+            })
+            const trackedEntityAttributes = response?.sources?.programTrackedEntityAttributes?.map(({trackedEntityAttribute}) => trackedEntityAttribute)
+            const dataElements = response?.dataElements?.programDataElements?.map(({dataElement}) => dataElement);
+            return {
+                data: [..._filter(trackedEntityAttributes, ['valueType', 'NUMBER']), ...dataElements],
+                pager: response?.dataElements?.pager
+
+            }
+        }
+        return {
+            data: [],
+            pager: {}
+        }
+    }
+
+    async filter(engine, {page, selectedGroup}) {
+        return this.getDataSources(engine, {page, filter: selectedGroup?.id})
+    }
 }
