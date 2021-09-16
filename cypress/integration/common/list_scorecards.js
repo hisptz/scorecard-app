097aa37f1ff306338f42c61b6a4c621e33639cc1
Danford Kija <kijadanford@gmail.com>
2021-09-16 14:10:35 +0300

    add fixtures on test

--- cypress/integration/common/list_scorecards.js
+++ cypress/integration/common/list_scorecards.js
@@ -1,17 +1,21 @@
 /// <reference types ="cypress" />
 
-// When("opening a list of available scorecards", () => {
-//     cy.intercept(
-//       "GET",
-//       Cypress.env("dhis2BaseUrl") +
-//         "/api/36/dataStore/hisptz-scorecard/savedObjects",
-//       { fixture: "" }
-//     );
+When("opening a list of available scorecards", () => {
+    // cy.intercept(
+    //   "GET",
+    //   Cypress.env("dhis2BaseUrl") +
+    //     "/api/36/dataStore/hisptz-scorecard/savedObjects",
+    //   { fixture: "" }
+    // );
   
-//     cy.intercept("GET", "/api/36/dataStore/hisptz-scorecard/scorecard-summary", {
-//       fixture: "scorecard-summary.json",
-//     });
-//   });
+    cy.intercept("GET", "/api/36/dataStore/hisptz-scorecard/scorecard-summary", {
+      fixture: "scorecard-summary.json",
+    }).as('score');
+    cy.wait('@score').then(()=>{
+      cy.get('[data-test=scorecard-thumbnail-view]').should("be.visible");
+    })
+
+  });
 
 //   When("opening a list of available scorecards", () => {
 //     cy
@@ -28,11 +32,11 @@
 //       expect(scoreboard).to.have.length.greaterThan(0)
 //     })
 //   });
-  When('opening a list of available scorecards',()=>{
-    cy.wait(6000)
-    cy.get('[data-test=scorecard-thumbnail-view]').should("be.visible");
+  // When('opening a list of available scorecards',()=>{
+  //   cy.wait(6000)
+  //   cy.get('[data-test=scorecard-thumbnail-view]').should("be.visible");
   
-  });
+  // });
   
 
 Then('a table of indicators against locations should displayed',()=>{
