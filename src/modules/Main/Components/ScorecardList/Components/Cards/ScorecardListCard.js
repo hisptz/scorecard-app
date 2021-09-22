4e10a165d25d86ada1c7ca5a71e56022e738fbb8
nnkogift <nnkogift@hotmail.com>
2021-09-22 16:29:26 +0300

    fix(UI ): Improvements
    - Styled scorecard list cards as per the suggestions from UiO

--- src/modules/Main/Components/ScorecardList/Components/Cards/ScorecardListCard.js
+++ src/modules/Main/Components/ScorecardList/Components/Cards/ScorecardListCard.js
@@ -4,7 +4,6 @@ import {Button, ButtonStrip, colors} from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React, {useState} from 'react'
 import {useHistory} from "react-router-dom";
-import holderImage from '../../../../../../resources/images/img.png'
 import DeleteConfirmation from "../../../../../../shared/Components/DeleteConfirmation";
 import {useDeleteScorecard} from "../../../../../../shared/hooks/datastore/useScorecard";
 
@@ -24,9 +23,9 @@ export default function ScorecardListCard({scorecard}) {
     }
 
     const onDelete = async () => {
-        try{
-           await remove()
-        } catch (e){
+        try {
+            await remove()
+        } catch (e) {
             show({
                 message: e.message,
                 type: {info: true}
@@ -42,41 +41,41 @@ export default function ScorecardListCard({scorecard}) {
 
     return (
         <div className='container-bordered p-32' style={{margin: 16, background: 'white'}}
-        onClick={onView}
+             onClick={onView}
         >
             <div className='row space-between align-items-center'>
-                <div className='row' >
-                    <img alt='img' src={holderImage} style={{height: 100, width: 200, paddingRight: 32}}/>
-                   <div className='column start'>
-                       <h3>{title}</h3>
-                       <p style={{color: colors.grey600, margin: 0}}>{description}</p>
-                   </div>
+                <div className='row'>
+                    <div className='column start'>
+                        <h4 className='scorecard-list-card-title'>{title}</h4>
+                        <p className="scorecard-list-card-description" style={{color: colors.grey700}}>{description}</p>
+                    </div>
                 </div>
                 <div className='row end'>
                     <ButtonStrip middle>
-                        <Button onClick={onView} >{i18n.t('View')}</Button>
-                        <Button onClick={function(_,e){
-                          e.stopPropagation()
-                             onEdit()
+                        <Button onClick={onView}>{i18n.t('View')}</Button>
+                        <Button onClick={function (_, e) {
+                            e.stopPropagation()
+                            onEdit()
                         }}>{i18n.t('Edit')}</Button>
-                        <Button onClick={function(_,e){
-                          e.stopPropagation()
-                          setDeleteConfirmOpen(true)
-                        }} >{i18n.t('Delete')}</Button>
+                        <Button onClick={function (_, e) {
+                            e.stopPropagation()
+                            setDeleteConfirmOpen(true)
+                        }}>{i18n.t('Delete')}</Button>
                     </ButtonStrip>
                     {
                         deleteConfirmOpen &&
-                        <DeleteConfirmation component={<p>{i18n.t('Are you sure you want to delete scorecard')}:<b>{title}</b></p>}
-                        onConfirm={function(_,e){
-                            e.stopPropagation()
-                            onDelete()
-                          }}
-                          onCancel={function(_,e){
-                            e.stopPropagation()
-                            setDeleteConfirmOpen(false)
-                          } }
+                        <DeleteConfirmation
+                            component={<p>{i18n.t('Are you sure you want to delete scorecard')}:<b>{title}</b></p>}
+                            onConfirm={function (_, e) {
+                                e.stopPropagation()
+                                onDelete()
+                            }}
+                            onCancel={function (_, e) {
+                                e.stopPropagation()
+                                setDeleteConfirmOpen(false)
+                            }}
 
-                                            />
+                        />
                     }
                 </div>
             </div>
