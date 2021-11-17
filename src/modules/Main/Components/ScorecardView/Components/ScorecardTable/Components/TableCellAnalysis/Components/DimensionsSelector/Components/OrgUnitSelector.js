2b879374c47ced1cb1bdbf54a646b993a0b8343d
Gift Nnko <nnkogift@gmail.com>
2021-11-17 07:25:14 +0300

    test(Add Scorecard): Fixes
    - Fixed styling issues

--- src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableCellAnalysis/Components/DimensionsSelector/Components/OrgUnitSelector.js
+++ src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableCellAnalysis/Components/DimensionsSelector/Components/OrgUnitSelector.js
@@ -1,46 +1,43 @@
 import i18n from "@dhis2/d2-i18n";
+import {OrgUnitSelectorModal} from "@hisptz/react-ui";
 import OrgUnitIcon from "@material-ui/icons/AccountTree";
-import React, { useMemo, useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import {
-  OrgUnitGroups,
-  OrgUnitLevels,
-} from "../../../../../../../../../../../core/state/orgUnit";
-import OrgUnitSelectorModal from "../../../../../../../../../../../shared/Components/OrgUnitSelectorModal";
+import React, {useMemo, useState} from "react";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {OrgUnitGroups, OrgUnitLevels,} from "../../../../../../../../../../../core/state/orgUnit";
 import getSelectedOrgUnitSelectionDisplay from "../../../../../../../../../../../shared/utils/orgUnit";
-import { OrgUnitState } from "../../../state/orgUnit";
+import {OrgUnitState} from "../../../state/orgUnit";
 import SelectionWrapper from "./SelectionWrapper";
 
 export default function OrgUnitSelector() {
-  const [orgUnitSelection, onChange] = useRecoilState(OrgUnitState);
-  const orgUnitLevels = useRecoilValue(OrgUnitLevels);
-  const orgUnitGroups = useRecoilValue(OrgUnitGroups);
-  const [selectorOpen, setSelectorOpen] = useState(false);
+    const [orgUnitSelection, onChange] = useRecoilState(OrgUnitState);
+    const orgUnitLevels = useRecoilValue(OrgUnitLevels);
+    const orgUnitGroups = useRecoilValue(OrgUnitGroups);
+    const [selectorOpen, setSelectorOpen] = useState(false);
 
-  const orgUnitSelectionDisplay = useMemo(
-    () =>
-      getSelectedOrgUnitSelectionDisplay(orgUnitSelection, {
-        orgUnitGroups,
-        orgUnitLevels,
-      }),
-    [orgUnitGroups, orgUnitLevels, orgUnitSelection]
-  );
+    const orgUnitSelectionDisplay = useMemo(
+        () =>
+            getSelectedOrgUnitSelectionDisplay(orgUnitSelection, {
+                orgUnitGroups,
+                orgUnitLevels,
+            }),
+        [orgUnitGroups, orgUnitLevels, orgUnitSelection]
+    );
 
-  return (
-    <div className="pr-16" style={{ width: "30%" }}>
-      <SelectionWrapper
-        label={i18n.t("Organisation Unit(s)")}
-        icon={OrgUnitIcon}
-        onClick={setSelectorOpen}
-        selectedItems={orgUnitSelectionDisplay}
-      />
-      {selectorOpen && (
-        <OrgUnitSelectorModal
-          onClose={() => setSelectorOpen(false)}
-          onSelect={onChange}
-          initialValue={orgUnitSelection}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div className="pr-16" style={{width: "30%"}}>
+            <SelectionWrapper
+                label={i18n.t("Organisation Unit(s)")}
+                icon={OrgUnitIcon}
+                onClick={setSelectorOpen}
+                selectedItems={orgUnitSelectionDisplay}
+            />
+            {
+                selectorOpen && <OrgUnitSelectorModal
+                    onClose={() => setSelectorOpen(false)}
+                    onSelect={onChange}
+                    initialValue={orgUnitSelection}
+                    hide={!selectorOpen} onUpdate={onChange}/>
+            }
+        </div>
+    );
 }
--- src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableHeader/Components/PeriodHeaderRow.js
+++ src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableHeader/Components/PeriodHeaderRow.js
@@ -1,88 +1,89 @@
-import { DataTableColumnHeader, DataTableRow } from "@dhis2/ui";
-import { head } from "lodash";
+import {DataTableColumnHeader, DataTableRow} from "@dhis2/ui";
+import {head} from "lodash";
 import PropTypes from "prop-types";
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { Orientation } from "../../../../../../../../../core/constants/orientation";
-import { PeriodResolverState } from "../../../../../../../../../core/state/period";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {Orientation} from "../../../../../../../../../core/constants/orientation";
+import {PeriodResolverState} from "../../../../../../../../../core/state/period";
 import {
-  ScorecardOrgUnitState,
-  ScorecardTableOrientationState,
-  ScorecardTableSortState,
-  ScorecardViewState,
+    ScorecardOrgUnitState,
+    ScorecardTableOrientationState,
+    ScorecardTableSortState,
+    ScorecardViewState,
 } from "../../../../../../../../../core/state/scorecard";
+import classes from '../../../scorecardTable.module.css'
 
-export default function PeriodHeaderRow({ orgUnits }) {
-  const { dataGroups } =
+export default function PeriodHeaderRow({orgUnits}) {
+    const {dataGroups} =
     useRecoilValue(ScorecardViewState("dataSelection")) ?? {};
-  const { filteredOrgUnits, childrenOrgUnits } =
+    const {filteredOrgUnits, childrenOrgUnits} =
     useRecoilValue(ScorecardOrgUnitState(orgUnits)) ?? {};
-  const orientation = useRecoilValue(ScorecardTableOrientationState);
-  const periods = useRecoilValue(PeriodResolverState) ?? [];
-  const [{ name: sortName, direction }, setDataSort] = useRecoilState(
-    ScorecardTableSortState
-  );
+    const orientation = useRecoilValue(ScorecardTableOrientationState);
+    const periods = useRecoilValue(PeriodResolverState) ?? [];
+    const [{name: sortName, direction}, setDataSort] = useRecoilState(
+        ScorecardTableSortState
+    );
 
-  const onSortClick = (direction) => {
-    setDataSort({
-      ...direction,
-      type: "period",
-    });
-  };
+    const onSortClick = (direction) => {
+        setDataSort({
+            ...direction,
+            type: "period",
+        });
+    };
 
-  return (
-    <DataTableRow>
-      {orientation === Orientation.ORG_UNIT_VS_DATA
-        ? dataGroups?.map(({ dataHolders }) =>
-            dataHolders?.map(({ id, dataSources }) =>
-              periods?.map(({ name, id: periodId }) => (
-                <DataTableColumnHeader
-                  dataTest={"test-period-table-scorecard"}
-                  fixed
-                  top={"0"}
-                  onSortIconClick={onSortClick}
-                  sortDirection={
-                    sortName === `${head(dataSources)?.id}-${periodId}`
-                      ? direction
-                      : "default"
-                  }
-                  width={"100px"}
-                  bordered
-                  align="center"
-                  key={`${id}-${periodId}`}
-                  className="scorecard-table-cell header"
-                  name={`${head(dataSources)?.id}-${periodId}`}
-                >
-                  {name}
-                </DataTableColumnHeader>
-              ))
-            )
-          )
-        : [...filteredOrgUnits, ...childrenOrgUnits]?.map(({ id }) =>
-            periods?.map(({ name, id: periodId }) => (
-              <DataTableColumnHeader
-                dataTest={"test-period-table-scorecard"}
-                fixed
-                top={"0"}
-                onSortIconClick={onSortClick}
-                sortDirection={
-                  sortName === `${id}-${periodId}` ? direction : "default"
-                }
-                width={"100px"}
-                bordered
-                align="center"
-                key={`${id}-${periodId}`}
-                className="scorecard-table-cell header"
-                name={`${id}-${periodId}`}
-              >
-                {name}
-              </DataTableColumnHeader>
-            ))
-          )}
-    </DataTableRow>
-  );
+    return (
+        <DataTableRow>
+            {orientation === Orientation.ORG_UNIT_VS_DATA
+                ? dataGroups?.map(({dataHolders}) =>
+                    dataHolders?.map(({id, dataSources}) =>
+                        periods?.map(({name, id: periodId}) => (
+                            <DataTableColumnHeader
+                                dataTest={"test-period-table-scorecard"}
+                                fixed
+                                top={"0"}
+                                onSortIconClick={onSortClick}
+                                sortDirection={
+                                    sortName === `${head(dataSources)?.id}-${periodId}`
+                                        ? direction
+                                        : "default"
+                                }
+                                width={"100px"}
+                                bordered
+                                align="center"
+                                key={`${id}-${periodId}`}
+                                className={classes["period-header-cell"]}
+                                name={`${head(dataSources)?.id}-${periodId}`}
+                            >
+                                {name}
+                            </DataTableColumnHeader>
+                        ))
+                    )
+                )
+                : [...filteredOrgUnits, ...childrenOrgUnits]?.map(({id}) =>
+                    periods?.map(({name, id: periodId}) => (
+                        <DataTableColumnHeader
+                            dataTest={"test-period-table-scorecard"}
+                            fixed
+                            top={"0"}
+                            onSortIconClick={onSortClick}
+                            sortDirection={
+                                sortName === `${id}-${periodId}` ? direction : "default"
+                            }
+                            width={"100px"}
+                            bordered
+                            align="center"
+                            key={`${id}-${periodId}`}
+                            className="scorecard-table-cell header"
+                            name={`${id}-${periodId}`}
+                        >
+                            {name}
+                        </DataTableColumnHeader>
+                    ))
+                )}
+        </DataTableRow>
+    );
 }
 
 PeriodHeaderRow.propTypes = {
-  orgUnits: PropTypes.arrayOf(PropTypes.object).isRequired,
+    orgUnits: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
