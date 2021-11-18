e8b359414c14112076adf6233e93007c61c79d23
Gift Nnko <nnkogift@gmail.com>
2021-11-18 12:54:23 +0300

    fix(Period Selection Loading): Fixes
    - Fixed issues with change in period or org unit or refreshing causes
    the loading indicator to load all the page instead of just the scorecard
    
    Closes #412

--- src/modules/Main/Components/ScorecardView/index.js
+++ src/modules/Main/Components/ScorecardView/index.js
@@ -1,18 +1,18 @@
-import { isEmpty } from "lodash";
-import React, { lazy, Suspense, useEffect, useMemo, useRef } from "react";
-import { useParams } from "react-router-dom";
-import { useRecoilCallback, useRecoilValue, useSetRecoilState } from "recoil";
+import {isEmpty} from "lodash";
+import React, {lazy, Suspense, useEffect, useMemo, useRef} from "react";
+import {useParams} from "react-router-dom";
+import {useRecoilCallback, useRecoilValue, useRecoilValueLoadable, useSetRecoilState} from "recoil";
 import ScorecardDataEngine from "../../../../core/models/scorecardData";
-import { InitialOrgUnits } from "../../../../core/state/orgUnit";
-import { PeriodResolverState } from "../../../../core/state/period";
+import {InitialOrgUnits} from "../../../../core/state/orgUnit";
+import {PeriodResolverState} from "../../../../core/state/period";
 import {
-  ScorecardDataSourceState,
-  ScorecardIdState,
-  ScorecardTableOrientationState,
-  ScorecardViewState,
+    ScorecardDataSourceState,
+    ScorecardIdState,
+    ScorecardTableOrientationState,
+    ScorecardViewState,
 } from "../../../../core/state/scorecard";
-import { UserAuthorityOnScorecard } from "../../../../core/state/user";
-import { FullPageLoader } from "../../../../shared/Components/Loaders";
+import {UserAuthorityOnScorecard} from "../../../../core/state/user";
+import {FullPageLoader} from "../../../../shared/Components/Loaders";
 import AccessDeniedPage from "./Components/AccessDeniedPage";
 import EmptyOrgUnitsOrPeriod from "./Components/EmptyOrgUnitsOrPeriod";
 import HighlightedIndicatorsView from "./Components/HighlightedIndicatorsView";
@@ -21,74 +21,79 @@ import ScorecardHeader from "./Components/ScorecardHeader";
 import ScorecardViewHeader from "./Components/ScorecardViewHeader";
 
 const ScorecardLegendsView = lazy(() =>
-  import("./Components/ScorecardLegendsView")
+    import("./Components/ScorecardLegendsView")
 );
 const ScorecardTable = lazy(() => import("./Components/ScorecardTable"));
 
 export default function ScorecardView() {
-  const { id: scorecardId } = useParams();
-  const setScorecardIdState = useSetRecoilState(ScorecardIdState);
-  const { orgUnits } = useRecoilValue(InitialOrgUnits);
-  const orgUnitsIds = useMemo(() => orgUnits?.map(({ id }) => id), [orgUnits]);
-  const { read: access } = useRecoilValue(
-    UserAuthorityOnScorecard(scorecardId)
-  );
-  const downloadRef = useRef();
-  const periods = useRecoilValue(PeriodResolverState);
+    const {id: scorecardId} = useParams();
+    const setScorecardIdState = useSetRecoilState(ScorecardIdState);
+    const orgUnitState = useRecoilValueLoadable(InitialOrgUnits);
+    const orgUnitsIds = useMemo(() => {
+        if (orgUnitState.state === 'hasValue') {
+            return orgUnitState.contents?.orgUnits?.map(orgUnit => orgUnit?.id) ?? [];
+        }
+        return [];
+    }, [orgUnitState.state, orgUnitState.contents]);
+    const {read: access} = useRecoilValue(
+        UserAuthorityOnScorecard(scorecardId)
+    );
+    const downloadRef = useRef();
+    const periods = useRecoilValue(PeriodResolverState);
 
-  const initialDataEngine = useMemo(() => new ScorecardDataEngine(), []);
+    const initialDataEngine = useMemo(() => new ScorecardDataEngine(), []);
 
-  const reset = useRecoilCallback(({ reset }) => () => {
-    reset(ScorecardViewState("periodSelection"));
-    reset(ScorecardViewState("tableSort"));
-    reset(ScorecardViewState("options"));
-    reset(ScorecardIdState);
-    reset(ScorecardTableOrientationState);
-    reset(ScorecardDataSourceState);
-  });
+    const reset = useRecoilCallback(({reset}) => () => {
+        reset(ScorecardViewState("periodSelection"));
+        reset(ScorecardViewState("tableSort"));
+        reset(ScorecardViewState("options"));
+        reset(ScorecardIdState);
+        reset(ScorecardTableOrientationState);
+        reset(ScorecardDataSourceState);
+    });
 
-  useEffect(() => {
-    setScorecardIdState(scorecardId);
-    return () => {
-      reset();
-    };
-  }, []);
+    useEffect(() => {
+        setScorecardIdState(scorecardId);
+        return () => {
+            reset();
+        };
+    }, []);
 
-  if (!access) {
-    return <AccessDeniedPage accessType={"view"} />;
-  }
+    if (!access) {
+        return <AccessDeniedPage accessType={"view"}/>;
+    }
 
-  return (
-    <Suspense fallback={<FullPageLoader />}>
-      <ScorecardViewHeader />
-      <Suspense fallback={<FullPageLoader />}>
-        <div
-          ref={downloadRef}
-          className="column p-16"
-          style={{ height: "100%", width: "100%", overflow: "auto" }}
-        >
-          <ScorecardActions
-            dataEngine={initialDataEngine}
-            downloadAreaRef={downloadRef}
-          />
-          <ScorecardHeader />
-          <ScorecardLegendsView />
-          <HighlightedIndicatorsView />
-          <div className="column align-items-center pt-16 flex-1">
-            {!isEmpty(orgUnits) && !isEmpty(periods) ? (
-              <Suspense fallback={<FullPageLoader />}>
-                <ScorecardTable
-                  initialDataEngine={initialDataEngine}
-                  nested={false}
-                  orgUnits={orgUnitsIds}
-                />
-              </Suspense>
-            ) : (
-              <EmptyOrgUnitsOrPeriod />
-            )}
-          </div>
-        </div>
-      </Suspense>
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<FullPageLoader/>}>
+            <ScorecardViewHeader/>
+            <Suspense fallback={<FullPageLoader/>}>
+                <div
+                    ref={downloadRef}
+                    className="column p-16"
+                    style={{height: "100%", width: "100%", overflow: "auto"}}
+                >
+                    <ScorecardActions
+                        dataEngine={initialDataEngine}
+                        downloadAreaRef={downloadRef}
+                    />
+                    <ScorecardHeader/>
+                    <ScorecardLegendsView/>
+                    <HighlightedIndicatorsView/>
+                    <div className="column align-items-center pt-16 flex-1">
+                        {!isEmpty(orgUnitState?.contents?.orgUnits) && !isEmpty(periods) ? (
+                            <Suspense fallback={<FullPageLoader small/>}>
+                                <ScorecardTable
+                                    initialDataEngine={initialDataEngine}
+                                    nested={false}
+                                    orgUnits={orgUnitsIds}
+                                />
+                            </Suspense>
+                        ) : (
+                            <EmptyOrgUnitsOrPeriod/>
+                        )}
+                    </div>
+                </div>
+            </Suspense>
+        </Suspense>
+    );
 }
--- src/shared/Components/Loaders/FullPageLoader.js
+++ src/shared/Components/Loaders/FullPageLoader.js
@@ -1,20 +1,21 @@
-import { CenteredContent, CircularLoader, colors } from "@dhis2/ui";
+import {CenteredContent, CircularLoader, colors} from "@dhis2/ui";
 import PropTypes from "prop-types";
 import React from "react";
 
-export default function FullPageLoader({ text }) {
-  return (
-    <div className="column center" style={{ height: "100%", width: '100%' }}>
-      <CenteredContent>
-        <div>
-          <CircularLoader />
-          <p style={{ color: colors.default }}>{text}</p>
+export default function FullPageLoader({text, small}) {
+    return (
+        <div className="column center" style={{height: "100%", width: '100%'}}>
+            <CenteredContent>
+                <div>
+                    <CircularLoader small={small}/>
+                    <p style={{color: colors.default}}>{text}</p>
+                </div>
+            </CenteredContent>
         </div>
-      </CenteredContent>
-    </div>
-  );
+    );
 }
 
 FullPageLoader.propTypes = {
-  text: PropTypes.string,
+    small: PropTypes.bool,
+    text: PropTypes.string,
 };
