3ae3dca69a82aa1dd6757cae62a5b082ee70e2d3
Gift Nnko <nnkogift@gmail.com>
2021-11-23 07:54:44 +0300

    fix(Data Configuration ): Fixes
    - Fixed issues with indicator configuration where the indicator clicked
    does not appear on the configuration area

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,33 @@
-const { config } = require("@dhis2/cli-style");
+const {config} = require("@dhis2/cli-style");
 
 module.exports = {
-  extends: [
-    config.eslintReact,
-    "plugin:cypress/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: ["cypress", "react-hooks"],
-  rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": [
-      "warn",
-      {
-        additionalHooks: "useRecoilCallback|useRecoilTransaction_UNSTABLE",
-      },
+    extends: [
+        config.eslintReact,
+        "plugin:cypress/recommended",
+        "plugin:react-hooks/recommended",
     ],
-  },
-  env: {
-    "cypress/globals": true,
-  },
+    plugins: ["cypress", "react-hooks"],
+    rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": [
+            "warn",
+            {
+                additionalHooks: "useRecoilCallback|useRecoilTransaction_UNSTABLE",
+            },
+        ],
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                "js": "never",
+                "jsx": "never",
+                "ts": "never",
+                "tsx": "never"
+            }
+        ]
+
+    },
+    env: {
+        "cypress/globals": true,
+    }
 };
