775a8a3d27bd39f5ad3778f86cdd468518ed9bc6
Gift Nnko <nnkogift@hotmail.com>
2022-01-28 13:42:57 +0300

    fix(General configuration): Implementation - Implemented react hook form for options configuration
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/modules/Main/Components/ScoreCardManagement/Components/Options/index.js
+++ src/modules/Main/Components/ScoreCardManagement/Components/Options/index.js
@@ -1,37 +1,17 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import ScorecardOptions from "../../../../../../core/models/scorecardOptions";
-import { ScorecardConfigDirtyState } from "../../../../../../core/state/scorecard";
 import ScorecardOptionsForm from "../../../../../../shared/Components/ScorecardOptionsForm";
+import useOptionsManage from "./hooks/useOptionsManage";
 
 export default function OptionsScorecardForm() {
-  const [scorecardOptions, setScorecardOptions] = useRecoilState(
-    ScorecardConfigDirtyState("options")
-  );
 
-  const onChange = (key) => (newValue) => {
-    setScorecardOptions((prevState) => {
-      return ScorecardOptions.set(
-        prevState,
-        key,
-        newValue?.checked ?? newValue
-      );
-    });
-  };
-
-  const onAverageChange = (value) => {
-    setScorecardOptions((prevState) =>
-      ScorecardOptions.set(prevState, "averageDisplayType", value)
+    const {scorecardOptions, onChange, onAverageChange} = useOptionsManage();
+    return (
+        <div style={{height: '100%'}}>
+            <ScorecardOptionsForm
+                options={scorecardOptions}
+                onChange={onChange}
+                onAverageChange={onAverageChange}
+            />
+        </div>
     );
-  };
-
-  return (
-    <div style={{ height: '100%'}}>
-      <ScorecardOptionsForm
-        options={scorecardOptions}
-        onChange={onChange}
-        onAverageChange={onAverageChange}
-      />
-    </div>
-  );
 }
