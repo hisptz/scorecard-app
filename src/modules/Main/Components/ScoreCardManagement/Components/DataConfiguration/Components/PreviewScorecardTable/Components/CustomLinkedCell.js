08fb05186e8f39643447a59d03c7bef7df317fa7
Gift Nnko <nnkogift@hotmail.com>
2022-02-11 10:47:57 +0300

    feat(Configuration): Specific targets implementation
     - Implemented specific targets view logic
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/modules/Main/Components/ScoreCardManagement/Components/DataConfiguration/Components/PreviewScorecardTable/Components/CustomLinkedCell.js
+++ src/modules/Main/Components/ScoreCardManagement/Components/DataConfiguration/Components/PreviewScorecardTable/Components/CustomLinkedCell.js
@@ -1,46 +1,50 @@
 import PropTypes from "prop-types";
-import React, { useMemo } from "react";
+import React, {useMemo} from "react";
+import {useFormContext} from "react-hook-form";
 import LinkedCellSvg from "../../../../../../../../../shared/Components/ScorecardCell/Components/LinkedCellSvg";
-import {
-  generateRandomValues,
-  getLegend,
-} from "../../../../../../../../../shared/utils/utils";
+import {generateRandomValues, getLegend,} from "../../../../../../../../../shared/utils/utils";
 
-export default function CustomLinkedCell({ top, bottom }) {
-  const {
-    legends: topLegends,
-    showColors: showTopColors,
-    displayArrows: topDisplayArrows,
-    weight: topWeight,
-  } = top ?? {};
-  const {
-    legends: bottomLegends,
-    showColors: showBottomColors,
-    displayArrows: bottomDisplayArrows,
-    weight: bottomWeight,
-  } = bottom ?? {};
-  const topValue = useMemo(generateRandomValues, []);
-  const bottomValue = useMemo(generateRandomValues, []);
-  const { color: topColor } =
-    getLegend(topValue, topLegends, { max: topWeight }) ?? {};
-  const { color: bottomColor } =
-    getLegend(bottomValue, bottomLegends, { max: bottomWeight }) ?? {};
+export default function CustomLinkedCell({top, bottom, orgUnit, period }) {
+    const {
+        legends: topLegends,
+        showColors: showTopColors,
+        displayArrows: topDisplayArrows,
+        weight: topWeight,
+        specificTargets: topSpecificTargets,
+    } = top ?? {};
+    const {
+        legends: bottomLegends,
+        showColors: showBottomColors,
+        displayArrows: bottomDisplayArrows,
+        weight: bottomWeight,
+        specificTargets: bottomSpecificTargets,
+    } = bottom ?? {};
+    const {watch} = useFormContext();
+    const legendDefinitions = watch("legendDefinitions");
+    const topValue = useMemo(generateRandomValues, []);
+    const bottomValue = useMemo(generateRandomValues, []);
+    const {color: topColor} =
+    getLegend(topValue, topLegends, {max: topWeight, specificTargets: topSpecificTargets, orgUnit, period, legendDefinitions }) ?? {};
+    const {color: bottomColor} =
+    getLegend(bottomValue, bottomLegends, {max: bottomWeight, specificTargets: bottomSpecificTargets, orgUnit, period, legendDefinitions}) ?? {};
 
-  return (
-    <td className="data-cell">
-      <LinkedCellSvg
-        topStatus={topDisplayArrows && "increasing"}
-        bottomStatus={bottomDisplayArrows && "decreasing"}
-        topColor={showTopColors ? topColor : undefined}
-        bottomColor={showBottomColors ? bottomColor : undefined}
-        bottomValue={bottomValue}
-        topValue={topValue}
-      />
-    </td>
-  );
+    return (
+        <td className="data-cell">
+            <LinkedCellSvg
+                topStatus={topDisplayArrows && "increasing"}
+                bottomStatus={bottomDisplayArrows && "decreasing"}
+                topColor={showTopColors ? topColor : undefined}
+                bottomColor={showBottomColors ? bottomColor : undefined}
+                bottomValue={bottomValue}
+                topValue={topValue}
+            />
+        </td>
+    );
 }
 
 CustomLinkedCell.propTypes = {
-  bottom: PropTypes.object.isRequired,
-  top: PropTypes.object.isRequired,
+    bottom: PropTypes.object.isRequired,
+    top: PropTypes.object.isRequired,
+    orgUnit: PropTypes.string,
+    period: PropTypes.string,
 };
--- src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableDataContainer/index.js
+++ src/modules/Main/Components/ScorecardView/Components/ScorecardTable/Components/TableDataContainer/index.js
@@ -1,206 +1,199 @@
 import i18n from "@dhis2/d2-i18n";
-import {
-  Menu,
-  MenuItem,
-  Popover,
-  IconVisualizationColumnStacked24,
-  IconVisualizationLine24,
-} from "@dhis2/ui";
-import { Period } from "@iapps/period-utilities";
+import {IconVisualizationColumnStacked24, IconVisualizationLine24, Menu, MenuItem, Popover,} from "@dhis2/ui";
+import {Period} from "@iapps/period-utilities";
 import PropTypes from "prop-types";
-import React, { useEffect, useState, useRef } from "react";
-import { useRecoilValue, useSetRecoilState,useResetRecoilState } from "recoil";
+import React, {useEffect, useRef, useState} from "react";
+import {useRecoilValue, useResetRecoilState, useSetRecoilState} from "recoil";
 import ScorecardDataEngine from "../../../../../../../../core/models/scorecardData";
-import { OrgUnitLevels } from "../../../../../../../../core/state/orgUnit";
-import {
-  ScorecardLegendDefinitionSelector,
-  ScorecardViewState,
-} from "../../../../../../../../core/state/scorecard";
-import { getLegend } from "../../../../../../../../shared/utils/utils";
+import {OrgUnitLevels} from "../../../../../../../../core/state/orgUnit";
+import {ScorecardViewState,} from "../../../../../../../../core/state/scorecard";
+import {getLegend} from "../../../../../../../../shared/utils/utils";
 import TableCellAnalysis from "../TableCellAnalysis";
-import {cellPeriodOptionSelector,cellPeriodOptionAtom} from "../TableCellAnalysis/state/period";
-import { LinkedDataCell, SingleDataCell } from "./Components/DataCells";
+import {cellPeriodOptionAtom, cellPeriodOptionSelector} from "../TableCellAnalysis/state/period";
+import {LinkedDataCell, SingleDataCell} from "./Components/DataCells";
 import LoadingCell from "./Components/LoadingCell";
+
 export default function DataContainer({
-  dataSources,
-  orgUnit,
-  period,
-  dataEngine,
-}) {
-  const orgUnitLevels = useRecoilValue(OrgUnitLevels);
-  const legendDefinitions = useRecoilValue(
-    ScorecardViewState("legendDefinitions")
-  );
-  const { id: orgUnitId, level: dataOrgUnitLevel } = orgUnit ?? {};
-  const { id: periodId } = period ?? {};
-  const defaultLegendDefinitions = useRecoilValue(
-    ScorecardLegendDefinitionSelector(true)
-  );
-  const [analysisOpen, setAnalysisOpen] = useState(false);
-  const [topData, setTopData] = useState();
-  const [bottomData, setBottomData] = useState();
-  const ref = useRef(null);
-  const [stateActionRef, setStateActionRef] = useState(null);
-  const [showSubMenu, setShowSubMenu] = React.useState(true)
- const resetPeriodsOptionSelection = useResetRecoilState(cellPeriodOptionAtom);
- const setPeriodOptionValueStates = useSetRecoilState(cellPeriodOptionSelector);
-  const [top, bottom] = dataSources ?? [];
-  const { color: topColor } =
+                                          dataSources,
+                                          orgUnit,
+                                          period,
+                                          dataEngine,
+                                      }) {
+    const orgUnitLevels = useRecoilValue(OrgUnitLevels);
+    const legendDefinitions = useRecoilValue(
+        ScorecardViewState("legendDefinitions")
+    );
+    const {id: orgUnitId, level: dataOrgUnitLevel} = orgUnit ?? {};
+    const {id: periodId} = period ?? {};
+    const [analysisOpen, setAnalysisOpen] = useState(false);
+    const [topData, setTopData] = useState();
+    const [bottomData, setBottomData] = useState();
+    const ref = useRef(null);
+    const [stateActionRef, setStateActionRef] = useState(null);
+    const [showSubMenu, setShowSubMenu] = React.useState(true)
+    const resetPeriodsOptionSelection = useResetRecoilState(cellPeriodOptionAtom);
+    const setPeriodOptionValueStates = useSetRecoilState(cellPeriodOptionSelector);
+    const [top, bottom] = dataSources ?? [];
+    const {color: topColor} =
     getLegend(topData?.current, top?.legends, {
-      max: top?.weight,
-      defaultLegends: defaultLegendDefinitions,
-      dataOrgUnitLevel,
-      orgUnitLevels,
-      legendDefinitions,
+        max: top?.weight,
+        dataOrgUnitLevel,
+        orgUnitLevels,
+        legendDefinitions,
+        period: periodId,
+        orgUnit: orgUnitId,
+        specificTargets: top?.specificTargets
     }) ?? {};
-  const { color: bottomColor } =
+    const {color: bottomColor} =
     getLegend(bottomData?.current, bottom?.legends, {
-      max: bottom?.weight,
-      defaultLegends: defaultLegendDefinitions,
-      dataOrgUnitLevel,
-      orgUnitLevels,
-      legendDefinitions,
+        max: bottom?.weight,
+        dataOrgUnitLevel,
+        orgUnitLevels,
+        legendDefinitions,
+        period: periodId,
+        orgUnit: orgUnitId,
+        specificTargets: bottom?.specificTargets
     }) ?? {};
 
-  const loading = false;
+    const loading = false;
 
-  const topKey = `${top.id}_${orgUnitId}_${periodId}`;
-  const bottomKey = `${bottom?.id}_${orgUnitId}_${periodId}`;
+    const topKey = `${top.id}_${orgUnitId}_${periodId}`;
+    const bottomKey = `${bottom?.id}_${orgUnitId}_${periodId}`;
 
-  useEffect(() => {
-    const topSub = dataEngine.get(topKey).subscribe((data) => {
-      setTopData(data);
-    });
-    const bottomSub = dataEngine.get(bottomKey).subscribe(setBottomData);
-    //Cleanup
-    return () => {
-      topSub.unsubscribe();
-      bottomSub.unsubscribe();
-      resetPeriodsOptionSelection()
-    };
-  }, [orgUnitId, periodId, top, bottom]);
+    useEffect(() => {
+        const topSub = dataEngine.get(topKey).subscribe((data) => {
+            setTopData(data);
+        });
+        const bottomSub = dataEngine.get(bottomKey).subscribe(setBottomData);
+        //Cleanup
+        return () => {
+            topSub.unsubscribe();
+            bottomSub.unsubscribe();
+            resetPeriodsOptionSelection()
+        };
+    }, [orgUnitId, periodId, top, bottom]);
 
-  return (
-    <>
-      <div
-        data-test={"data-cell"}
-        onClick={() => {
-          setAnalysisOpen(true);
-        }}
-        onContextMenu={(e) => {
-          e.preventDefault();
-          setStateActionRef(e.target);
-        }}
-        ref={ref}
-      >
-        {loading ? (
-          <LoadingCell />
-        ) : dataSources?.length > 1 ? (
-          <LinkedDataCell
-            topIndicator={top}
-            bottomIndicator={bottom}
-            bottomData={bottomData}
-            topData={topData}
-            bottomColor={bottomColor}
-            topColor={topColor}
-          />
-        ) : (
-          <SingleDataCell indicator={top} data={topData} color={topColor} />
-        )}
-      </div>
-      {analysisOpen && (
-        <TableCellAnalysis
-          orgUnit={orgUnit}
-          period={period}
-          dataHolder={{ dataSources }}
-          onClose={() => {
-            setAnalysisOpen(false);
-          }}
-        />
-      )}
-      {stateActionRef && (
-        <Popover
-          onClickOutside={() => setStateActionRef(undefined)}
-          placement="bottom-start"
-          reference={stateActionRef}
-        >
-          <Menu>
-            <MenuItem
-              onClick={() => {
-                setStateActionRef(undefined);
-                setAnalysisOpen(true);
-              }}
-              label={i18n.t("Further Analysis")}
-              icon={<IconVisualizationColumnStacked24 />}
-            />
-            <MenuItem
-              onClick={() => {
-                setStateActionRef(undefined);
-              }}
-              showSubMenu={showSubMenu}
-              toggleSubMenu={()=>{
-               return setShowSubMenu(!showSubMenu)
-              }}
-              label={i18n.t("Trend Analysis ")}
-              icon={<IconVisualizationLine24 />}
+    return (
+        <>
+            <div
+                data-test={"data-cell"}
+                onClick={() => {
+                    setAnalysisOpen(true);
+                }}
+                onContextMenu={(e) => {
+                    e.preventDefault();
+                    setStateActionRef(e.target);
+                }}
+                ref={ref}
             >
-              <MenuItem
-                onClick={() => {
-                  setStateActionRef(undefined);
-                  setPeriodOptionValueStates([ new Period().setPreferences({ allowFuturePeriods: true }).getById("LAST_3_MONTHS")])
-                  setAnalysisOpen(true);
-                }}
-                label={i18n.t("Last 3 Month")}
-                icon={<IconVisualizationColumnStacked24 />}
-              />
-              <MenuItem
-                onClick={() => {
-                  setStateActionRef(undefined);
-                  setPeriodOptionValueStates([ new Period().setPreferences({ allowFuturePeriods: true }).getById("LAST_6_MONTHS")])
-                  setAnalysisOpen(true);
-                }}
-                label={i18n.t("Last 6 Month")}
-                icon={<IconVisualizationColumnStacked24 />}
-              />
-              <MenuItem
-                onClick={() => {
-                  setStateActionRef(undefined);
-                  setPeriodOptionValueStates([ new Period().setPreferences({ allowFuturePeriods: true }).getById("LAST_12_MONTHS")])
-                  setAnalysisOpen(true);
-                }}
-                label={i18n.t("Last 12 Month")}
-                icon={<IconVisualizationColumnStacked24 />}
-              />
-                <MenuItem
-                onClick={() => {
-                  setStateActionRef(undefined);
-                  setPeriodOptionValueStates([ new Period().setPreferences({ allowFuturePeriods: true }).getById("LAST_4_QUARTERS")])
-                  setAnalysisOpen(true);
-                }}
-                label={i18n.t("Last 4 Quarter")}
-                icon={<IconVisualizationColumnStacked24 />}
-              />
-                <MenuItem
-                onClick={() => {
-                  setStateActionRef(undefined);
-                  setPeriodOptionValueStates([ new Period().setPreferences({ allowFuturePeriods: true }).getById("LAST_5_YEARS")])
-                  setAnalysisOpen(true);
-                }}
-                label={i18n.t("Last 5 Years")}
-                icon={<IconVisualizationColumnStacked24 />}
-              />
-            </MenuItem>
-            
-          </Menu>
-        </Popover>
-      )}
-    </>
-  );
+                {loading ? (
+                    <LoadingCell/>
+                ) : dataSources?.length > 1 ? (
+                    <LinkedDataCell
+                        topIndicator={top}
+                        bottomIndicator={bottom}
+                        bottomData={bottomData}
+                        topData={topData}
+                        bottomColor={bottomColor}
+                        topColor={topColor}
+                    />
+                ) : (
+                    <SingleDataCell indicator={top} data={topData} color={topColor}/>
+                )}
+            </div>
+            {analysisOpen && (
+                <TableCellAnalysis
+                    orgUnit={orgUnit}
+                    period={period}
+                    dataHolder={{dataSources}}
+                    onClose={() => {
+                        setAnalysisOpen(false);
+                    }}
+                />
+            )}
+            {stateActionRef && (
+                <Popover
+                    onClickOutside={() => setStateActionRef(undefined)}
+                    placement="bottom-start"
+                    reference={stateActionRef}
+                >
+                    <Menu>
+                        <MenuItem
+                            onClick={() => {
+                                setStateActionRef(undefined);
+                                setAnalysisOpen(true);
+                            }}
+                            label={i18n.t("Further Analysis")}
+                            icon={<IconVisualizationColumnStacked24/>}
+                        />
+                        <MenuItem
+                            onClick={() => {
+                                setStateActionRef(undefined);
+                            }}
+                            showSubMenu={showSubMenu}
+                            toggleSubMenu={() => {
+                                return setShowSubMenu(!showSubMenu)
+                            }}
+                            label={i18n.t("Trend Analysis ")}
+                            icon={<IconVisualizationLine24/>}
+                        >
+                            <MenuItem
+                                onClick={() => {
+                                    setStateActionRef(undefined);
+                                    setPeriodOptionValueStates([new Period().setPreferences({allowFuturePeriods: true}).getById("LAST_3_MONTHS")])
+                                    setAnalysisOpen(true);
+                                }}
+                                label={i18n.t("Last 3 Month")}
+                                icon={<IconVisualizationColumnStacked24/>}
+                            />
+                            <MenuItem
+                                onClick={() => {
+                                    setStateActionRef(undefined);
+                                    setPeriodOptionValueStates([new Period().setPreferences({allowFuturePeriods: true}).getById("LAST_6_MONTHS")])
+                                    setAnalysisOpen(true);
+                                }}
+                                label={i18n.t("Last 6 Month")}
+                                icon={<IconVisualizationColumnStacked24/>}
+                            />
+                            <MenuItem
+                                onClick={() => {
+                                    setStateActionRef(undefined);
+                                    setPeriodOptionValueStates([new Period().setPreferences({allowFuturePeriods: true}).getById("LAST_12_MONTHS")])
+                                    setAnalysisOpen(true);
+                                }}
+                                label={i18n.t("Last 12 Month")}
+                                icon={<IconVisualizationColumnStacked24/>}
+                            />
+                            <MenuItem
+                                onClick={() => {
+                                    setStateActionRef(undefined);
+                                    setPeriodOptionValueStates([new Period().setPreferences({allowFuturePeriods: true}).getById("LAST_4_QUARTERS")])
+                                    setAnalysisOpen(true);
+                                }}
+                                label={i18n.t("Last 4 Quarter")}
+                                icon={<IconVisualizationColumnStacked24/>}
+                            />
+                            <MenuItem
+                                onClick={() => {
+                                    setStateActionRef(undefined);
+                                    setPeriodOptionValueStates([new Period().setPreferences({allowFuturePeriods: true}).getById("LAST_5_YEARS")])
+                                    setAnalysisOpen(true);
+                                }}
+                                label={i18n.t("Last 5 Years")}
+                                icon={<IconVisualizationColumnStacked24/>}
+                            />
+                        </MenuItem>
+
+                    </Menu>
+                </Popover>
+            )}
+        </>
+    );
 }
 
 DataContainer.propTypes = {
-  dataEngine: PropTypes.instanceOf(ScorecardDataEngine).isRequired,
-  dataSources: PropTypes.array.isRequired,
-  orgUnit: PropTypes.object.isRequired,
-  period: PropTypes.object.isRequired,
+    dataEngine: PropTypes.instanceOf(ScorecardDataEngine).isRequired,
+    dataSources: PropTypes.array.isRequired,
+    orgUnit: PropTypes.object.isRequired,
+    period: PropTypes.object.isRequired,
 };
