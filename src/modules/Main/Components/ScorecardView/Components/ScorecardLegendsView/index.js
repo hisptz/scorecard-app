2484e4ec3126b98553f5bc01a62fc1a37cc27493
Gift Nnko <nnkogift@hotmail.com>
2022-02-22 10:40:08 +0300

    fix(Legend Definitions View): Styling
    - Improved styling for legend definitions on scorecard view
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/modules/Main/Components/ScorecardView/Components/ScorecardLegendsView/index.js
+++ src/modules/Main/Components/ScorecardView/Components/ScorecardLegendsView/index.js
@@ -26,34 +26,43 @@ export default function ScorecardLegendsView() {
             <div>
                 <h3>{i18n.t("Legends")}</h3>
             </div>
-            <div className="row space-between align-items-center gap-16">
-                <div className="row align-items-center ">
-                    {legends?.map((legend) => (
-                        <LegendView key={legend.color} legend={legend}/>
-                    ))}
+            <div  className="row align-items-center gap-16 space-between">
+                <div style={{
+                    flex: `0 0 ${showArrows ? "70%" : "90%"}`,
+                }} className="column">
+                    <div className="row align-items-center">
+                        {legends?.map((legend) => (
+                            <LegendView key={legend.color} legend={legend}/>
+                        ))}
+                    </div>
                 </div>
-                {
-                    showArrows && <div className="row align-items-center end">
-                        <div className="pr-16">
-                            <svg height={14} width={14}>
-                                <IncreasingArrows fontSize={14} y={0} x={7}/>
-                            </svg>
-                            &nbsp; {i18n.t("Increased from last period")}
-                        </div>
-                        <div>
-                            <svg height={14} width={14}>
-                                <DecreasingArrows fontSize={14} y={14} x={7}/>
-                            </svg>
-                            &nbsp; {i18n.t("Decreased from last period")}
+                <div className="column">
+                    <div className="row gap-16 space-between justify-content-end">
+                        {
+                            showArrows && <div style={{minWidth: 400}} className="row align-items-center end">
+                                <div className="pr-16">
+                                    <svg height={14} width={14}>
+                                        <IncreasingArrows fontSize={14} y={0} x={7}/>
+                                    </svg>
+                                    &nbsp; {i18n.t("Increased from last period")}
+                                </div>
+                                <div>
+                                    <svg height={14} width={14}>
+                                        <DecreasingArrows fontSize={14} y={14} x={7}/>
+                                    </svg>
+                                    &nbsp; {i18n.t("Decreased from last period")}
+                                </div>
+                            </div>
+                        }
+                        <div className="print-hide" style={{minWidth: 200}}>
+                            {
+                                isSpecificTargetsSet && <Button onClick={() => setSpecificTargetsLibraryOpen(true)}>
+                                    {i18n.t("Specific Targets Library")}
+                                </Button>
+                            }
                         </div>
                     </div>
-                }
-                <div className="print-hide" style={{minWidth: 200}}>
-                    {
-                        isSpecificTargetsSet && <Button onClick={() => setSpecificTargetsLibraryOpen(true)}>
-                            {i18n.t("Specific Targets Library")}
-                        </Button>
-                    }
+
                 </div>
                 {
                     specificTargetsLibraryOpen &&
