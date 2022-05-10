91d21d2d58dec6dbf945d44131189646b2f268b1
Gift <nnkogift@hotmail.com>
2022-05-10 10:59:55 +0300

    :test_tube: test(Tests): Fixes
    Fixes and improvements on test

--- apps/app/cypress/integration/0.7-Delete-Scorecard/delete-scorecard.js
+++ apps/app/cypress/integration/0.7-Delete-Scorecard/delete-scorecard.js
@@ -3,8 +3,8 @@ When(/^deleting scorecard$/, function () {
     cy.get('[data-test="scorecard-delete-button"]').first().click();
 });
 When(/^confirming to delete scorecard$/, function () {
-    cy.get('[data-test=delete-confirm-modal]').should('be.visible')
-    cy.get('button[data-test=delete-confirm-button]').click()
+    cy.get('[data-test="dhis2-uicore-card"]').should('be.visible')
+    cy.get('button[data-test="dhis2-uicore-button"]').contains("Confirm").click();
 
 });
 Then(/^the deleted scorecard should not be on the list$/, function () {
