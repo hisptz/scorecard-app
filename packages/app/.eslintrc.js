5fdb9876987ed05f1c6fd0e820d3901f938210e1
Davis799 <davistibz@gmail.com>
2024-07-17 21:56:16 +0300

    Migration to typescript

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,11 +1,21 @@
 const {config} = require("@dhis2/cli-style");
 
 module.exports = {
+    parser: "@typescript-eslint/parser",
     extends: [
         config.eslintReact,
         "plugin:cypress/recommended",
         "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended"
+        
     ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
     plugins: ["cypress", "react-hooks"],
     rules: {
         "react-hooks/rules-of-hooks": "error",
