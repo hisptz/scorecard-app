a6d03a207bf77750e2ba4dbdde08ee73f2edc990
Davis799 <davistibz@gmail.com>
2024-07-18 17:16:18 +0300

    Migration of src/components/OrgUnitFilter/hooks/useOrgUnitsRoot.ts to typescript

---
+++ packages/shared/src/components/OrgUnitFilter/hooks/useOrgUnitsRoot.ts
@@ -0,0 +1,25 @@
+import { apiFetchOrganisationUnitRoots } from "@dhis2/analytics";
+import { useDataEngine } from "@dhis2/app-runtime";
+import { useState, useEffect } from "react";
+
+export default function useOrgUnitsRoot() {
+	const engine = useDataEngine();
+	const [roots, setRoots] = useState();
+	const [loading, setLoading] = useState<boolean>();
+	const [error, setError] = useState<any>();
+
+	useEffect(() => {
+		async function getOrgUnits() {
+			try {
+				setLoading(true);
+				setRoots(await apiFetchOrganisationUnitRoots(engine));
+				setLoading(false);
+			} catch (e) {
+				setError(e);
+			}
+		}
+		getOrgUnits();
+	}, []);
+
+	return { roots, loading, error };
+}
