17d1b1c865f347031a8d6549fefa297b001b7834
Davis799 <davistibz@gmail.com>
2024-07-18 17:16:19 +0300

    Migration of src/components/ScorecardCell/Components/Arrows.tsx to typescript

---
+++ packages/shared/src/components/ScorecardCell/Components/Arrows.tsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+export const IncreasingArrows = ({ x, y, decreasing, fontSize }: any) => (
+	<path
+		d={`M ${x} ${y} L ${x - fontSize / 2} ${y + fontSize} L ${x +
+			fontSize / 2} ${y + fontSize} Z`}
+		fill="black"
+		transform={decreasing && "rotate(90)"}
+	/>
+);
+
+IncreasingArrows.propTypes = {
+	fontSize: PropTypes.number.isRequired,
+	x: PropTypes.number.isRequired,
+	y: PropTypes.number.isRequired,
+	decreasing: PropTypes.bool,
+};
+
+export const DecreasingArrows = ({ x, y, fontSize }: any) => (
+	<path
+		d={`M ${x} ${y} L ${x - fontSize / 2} ${y - fontSize} L ${x +
+			fontSize / 2} ${y - fontSize} Z`}
+		fill="black"
+	/>
+);
+
+DecreasingArrows.propTypes = {
+	fontSize: PropTypes.number.isRequired,
+	x: PropTypes.number.isRequired,
+	y: PropTypes.number.isRequired,
+};
