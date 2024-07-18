d02923021e15c61af14c269853ad583bde03faf4
Davis799 <davistibz@gmail.com>
2024-07-18 17:16:03 +0300

    Migration of src/constants/layout.js to typescript

--- packages/shared/src/constants/layout.js
+++ packages/shared/src/constants/layout.js
@@ -5,26 +5,26 @@ import OrgUnitIcon from "@material-ui/icons/AccountTree";
 import DataIcon from "@material-ui/icons/Storage";
 import React from "react";
 
-export const LAYOUTS = {
-  dx: {
-    name: "data",
-    displayName: i18n.t("Data"),
-    icon: <DataIcon style={{ color: colors.grey700 }} />,
-  },
-  ou: {
-    name: "orgUnit",
-    displayName: i18n.t("Organisation Unit"),
-    icon: <OrgUnitIcon style={{ color: colors.grey700 }} />,
-  },
-  pe: {
-    name: "period",
-    displayName: i18n.t("Period"),
-    icon: <PeriodIcon style={{ color: colors.grey700 }} />,
-  },
+export const LAYOUTS: any = {
+	dx: {
+		name: "data",
+		displayName: i18n.t("Data"),
+		icon: <DataIcon style={{ color: colors.grey700 }} />,
+	},
+	ou: {
+		name: "orgUnit",
+		displayName: i18n.t("Organisation Unit"),
+		icon: <OrgUnitIcon style={{ color: colors.grey700 }} />,
+	},
+	pe: {
+		name: "period",
+		displayName: i18n.t("Period"),
+		icon: <PeriodIcon style={{ color: colors.grey700 }} />,
+	},
 };
 
 export const DEFAULT_LAYOUT = {
-  column: ["dx"],
-  row: ["pe"],
-  filter: ["ou"],
+	column: ["dx"],
+	row: ["pe"],
+	filter: ["ou"],
 };
