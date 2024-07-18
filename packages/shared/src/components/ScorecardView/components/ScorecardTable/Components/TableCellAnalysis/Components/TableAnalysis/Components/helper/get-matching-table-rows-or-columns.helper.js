f87c033a0f8fce96c1642888dca0cc15e434f5f9
Davis799 <davistibz@gmail.com>
2024-07-18 17:19:21 +0300

    Migration to typescript

--- packages/shared/src/components/ScorecardView/components/ScorecardTable/Components/TableCellAnalysis/Components/TableAnalysis/Components/helper/get-matching-table-rows-or-columns.helper.js
+++
@@ -1,7 +0,0 @@
-import { filter } from "lodash";
-
-export function getMatchingTableRowsOrColumns(tableDataRowOrColumn) {
-  return filter(
-    filter(tableDataRowOrColumn, (tableDataCell) => tableDataCell.dataRowIds)
-  );
-}
