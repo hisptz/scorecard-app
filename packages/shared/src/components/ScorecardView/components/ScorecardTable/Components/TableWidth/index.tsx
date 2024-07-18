153c31847d0ec6684c22a599110016e540f3f584
Davis799 <davistibz@gmail.com>
2024-07-18 17:16:50 +0300

    Migration of src/components/ScorecardView/components/ScorecardTable/Components/TableWidth/index.tsx to typescript

---
+++ packages/shared/src/components/ScorecardView/components/ScorecardTable/Components/TableWidth/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { ScorecardTableConfigState } from "../../../../../../state";
+
+export default function TableWidth({ orgUnits }: any) {
+	const { nameColumnWidth } = useRecoilValue(
+		ScorecardTableConfigState(orgUnits),
+	);
+
+	return (
+		<colgroup>
+			<col width="50px" />
+			<col width="50px" />
+			<col width={`${nameColumnWidth}`} />
+		</colgroup>
+	);
+}
