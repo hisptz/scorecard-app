fd34880fac549efadc27fa3a87a052a6df30a7bb
Davis799 <davistibz@gmail.com>
2024-07-18 17:16:40 +0300

    Migration of src/components/ScorecardView/components/ScorecardTable/Components/TableCellAnalysis/Components/TableAnalysis/Components/helper/get-table-header-rows.helper.ts to typescript

---
+++ packages/shared/src/components/ScorecardView/components/ScorecardTable/Components/TableCellAnalysis/Components/TableAnalysis/Components/helper/get-table-header-rows.helper.ts
@@ -0,0 +1,46 @@
+import { each, map } from "lodash";
+
+// eslint-disable-next-line max-params
+export function getTableHeaderRows(
+	tableRow: any,
+	tableColumnsArray: string | any[],
+	analyticsObject: any,
+	tableConfiguration: any,
+) {
+	let tableHeaderRows: any[] = [];
+
+	each(tableColumnsArray, (tableColumn, tableColumnIndex) => {
+		const newTableColumn = map(
+			tableColumn,
+			(tableColumnCell, tableColumnCellIndex) => {
+				const columnDimension = tableConfiguration.column[tableColumnCellIndex];
+				return {
+					...tableColumnCell,
+					dimension: columnDimension,
+					name: tableColumnCell.name,
+				};
+			},
+		);
+		if (tableColumnIndex === 0) {
+			tableHeaderRows = [
+				...tableHeaderRows,
+				[
+					...map(tableRow, (tableRowCell, tableRowCellIndex) => {
+						const rowDimension = tableConfiguration.row[tableRowCellIndex];
+						return {
+							...tableRowCell,
+							dimension: rowDimension,
+							name: analyticsObject.metaData.names[rowDimension],
+							rowSpan: tableColumnsArray.length,
+						};
+					}),
+					...newTableColumn,
+				],
+			];
+		} else {
+			tableHeaderRows = [...tableHeaderRows, newTableColumn];
+		}
+	});
+
+	return tableHeaderRows;
+}
