2c3d679e552798c14ea4aa54c56f5504211fc225
Davis799 <davistibz@gmail.com>
2024-07-19 15:08:17 +0300

    Migration to typescript

--- packages/shared/src/services/index.js
+++
@@ -1,9 +0,0 @@
-export {default as getCustomFunctions} from "./getCustomFunctions";
-export {default as getScorecardCellData, getHighlightedIndicatorsData} from "./getScorecardCellData";
-export {default as getScorecardData} from "./getScorecardData";
-export {getOrgUnitsFromAnalytics, isOrgUnitId} from "./orgUnit"
-export {default as getSystemSettings} from "./getSystemSettings"
-export {default as getScorecard, getOrgUnitSelection} from "./getScorecard"
-export {default as getScorecardSummary, restoreScorecardSummary} from "./getScorecardSummary"
-
-
