5551e3e9016191f4887f1588618a4e46af8c8e2f
Davis799 <davistibz@gmail.com>
2024-07-19 15:05:21 +0300

    Migration to typescript

---
+++ packages/shared/src/models/orgUnitSelection.ts
@@ -0,0 +1,14 @@
+import DataModel from "./base";
+
+export default class OrgUnitSelection extends DataModel {
+	get defaults() {
+		return {
+			levels: [],
+			groups: [],
+			orgUnits: [],
+			userSubX2Unit: false,
+			userSubUnit: false,
+			userOrgUnit: false,
+		};
+	}
+}
