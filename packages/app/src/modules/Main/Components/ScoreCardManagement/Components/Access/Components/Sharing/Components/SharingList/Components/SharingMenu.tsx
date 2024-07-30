3c449941b6d3c617d5f4da9e167615d4e1f7bfde
Davis799 <davistibz@gmail.com>
2024-07-30 15:17:46 +0300

    feat(ui icons): feat
    -  Updated the ui icon from the dhis2 ui icons library

--- packages/app/src/modules/Main/Components/ScoreCardManagement/Components/Access/Components/Sharing/Components/SharingList/Components/SharingMenu.tsx
+++ packages/app/src/modules/Main/Components/ScoreCardManagement/Components/Access/Components/Sharing/Components/SharingList/Components/SharingMenu.tsx
@@ -1,6 +1,6 @@
 import { FlyoutMenu, Layer, MenuItem, Popper } from "@dhis2/ui";
 import { ACCESS_TYPES } from "@scorecard/shared";
-import CheckIcon from "@material-ui/icons/Check";
+import { IconCheckmark24 } from "@dhis2/ui-icons";
 import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 import { getAccessName } from "../../../utils";
@@ -13,10 +13,7 @@ export default function SharingMenu({
 }: any) {
 	return (
 		<Layer onClick={onClose}>
-			<Popper
-				reference={reference}
-				placement="bottom-start"
-			>
+			<Popper reference={reference} placement="bottom-start">
 				<FlyoutMenu>
 					{ACCESS_TYPES?.map((accessType: any) => (
 						<Fragment key={accessType}>
@@ -24,7 +21,7 @@ export default function SharingMenu({
 								onClick={() => onAccessChange(accessType)}
 								active={selectedAccess === accessType}
 								label={getAccessName(accessType)}
-								icon={selectedAccess === accessType && <CheckIcon />}
+								icon={selectedAccess === accessType && <IconCheckmark24 />}
 							/>
 						</Fragment>
 					))}
