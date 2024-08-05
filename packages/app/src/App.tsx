6682840b3800ecb0321177bdbc6f01a14dd23ed7
Davis799 <davistibz@gmail.com>
2024-08-05 16:47:43 +0300

    Implementing the new scorecard widget: App.tsx

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -7,9 +7,10 @@ import {
 	FullPageLoader,
 	useInitApp,
 } from "@scorecard/shared";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useLayoutEffect, useRef } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { RecoilRoot } from "recoil";
+import postRobot from "@krakenjs/post-robot";
 import "./media-queries.css";
 import "./App.css";
 import "./print.css";
@@ -17,11 +18,50 @@ import Router from "./modules/Router";
 import "./locales";
 import "intro.js/introjs.css";
 import "./intro-dhis2.css";
+import { isEmpty } from "lodash";
 
 const MyApp = () => {
 	const { initializeState } = useInitApp();
+	const iframeRef: any = useRef(null);
 
-	return (
+	const dashboardItemId: String | undefined = (
+		/[?&]dashboardItemId=([a-zA-Z0-9]{11})(?:&|$)/g.exec(
+			window.location.search,
+		) || [undefined]
+	).pop();
+
+	const sendDataToPlugin = (id: String) => {
+		const data = { dashboardItemId: id };
+		if (iframeRef.current && iframeRef.current.contentWindow) {
+			postRobot
+				.send(iframeRef.current.contentWindow, "dataFromParent", data)
+				.catch((error: any) => {
+					console.error("Error sending data:", error);
+				});
+		}
+	};
+
+	const hideHeader = () => {
+		const header = document.querySelector(
+			"#dhis2-app-root > div > div:nth-child(1)",
+		);
+		if (header) {
+			header.remove();
+		}
+	};
+	useLayoutEffect(() => {
+		if (dashboardItemId) {
+			hideHeader();
+		}
+	}, []);
+
+	useEffect(() => {
+		if (dashboardItemId) {
+			sendDataToPlugin(dashboardItemId);
+		}
+	}, [dashboardItemId]);
+
+	return isEmpty(dashboardItemId) ? (
 		<DataStoreProvider
 			namespace={DATASTORE_NAMESPACE}
 			loadingComponent={<FullPageLoader />}
@@ -39,6 +79,14 @@ const MyApp = () => {
 				</ErrorBoundary>
 			</RecoilRoot>
 		</DataStoreProvider>
+	) : (
+		<iframe
+			ref={iframeRef}
+			src="./plugin.html"
+			style={{ width: "100%", height: "99%", border: "none" }}
+			title="Interactive Scorecard Widget"
+			loading="lazy"
+		/>
 	);
 };
 
