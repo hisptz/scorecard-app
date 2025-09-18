b0e852b1cfecf9407d4f7b155d3c0ad0259a1715
Gift Nnko <nnkogift@gmail.com>
2025-09-18 14:37:15 +0300

    chore: setup ci for release and publishing to app hub

--- d2.config.js
+++ d2.config.js
@@ -1,4 +1,9 @@
+/**
+ * @type {import("@dhis2/cli-app-scripts").D2Config}
+ */
 const config = {
+	id: "6e3af2e6-6dac-49b8-baa1-40019a684252",
+	author: "HISP Tanzania",
 	type: "app",
 	name: "hisptz-scorecard",
 	title: "Scorecard",
@@ -9,13 +14,8 @@ const config = {
 	},
 	dataStoreNamespace: "hisptz-scorecard",
 	minDHIS2Version: "2.40",
+	maxDHIS2Version: "2.42",
 	direction: "auto",
-	pwa: {
-		enabled: true,
-		caching: {
-			patternsToOmitFromAppShell: [/.*/]
-		}
-	},
 	viteConfigExtensions: "./viteConfigExtensions.mts"
 };
 module.exports = config;
